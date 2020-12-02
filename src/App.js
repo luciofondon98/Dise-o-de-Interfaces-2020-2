bac543e21381500883393d528ba395731ebe872c
luciofondon98 <luciofondon98@gmail.com>
2020-12-02 17:57:18 -0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
 
+import { Router, Switch, Route } from "react-router-dom";
+import history from './helpers/history'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import NavBar from './components/navbar/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
+        <NavBar></NavBar>
         <Button>lucio chupalo</Button>
       </header>
     </div>
---
+++ src/components/navbar/routes.js
@@ -0,0 +1,16 @@
+export default function () {
+    return [ 
+        {
+            name: 'Inicio',
+            path: '/'
+        },
+        {
+            name: 'Mis Noticias',
+            path: '/mis-noticias'
+        },
+        {
+            name: 'Mi Perfil',
+            path: '/perfil'
+        },
+    ];
+};
